f96732d52f2baea08919a21e67d778001e155537
kaushik <kaushik@gmail.com>
2021-07-26 09:38:17 +0530

    Created Netflix-trailer app

---
+++ src/components/Row.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import YouTube from "react-youtube";
+import movieTrailer from 'movie-trailer';
+import axios from "../helpers/axios";
+import "./Row.css";
+
+
+const base_url = "https:/image.tmdb.org/t/p/original/";
+
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    console.log(movie);
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+        .then((url) => {
+          console.log("url:>> ",url);
+          const urlParams = new URLSearchParams(new URL(url).search)
+          setTrailerUrl(urlParams.get('v'));
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    };
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      {/* title */}
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row_poster && ${isLargeRow && "row_posterLarge"}`}
+            onClick={() => handleClick(movie)}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+};
+
+export default Row;
